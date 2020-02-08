de25591e9168186d805409cb34c58cf8baf27be2
deeztek <dino.edwards@mydirectmail.net>
2020-02-08 07:58:53 -0500

    Added /bin/rm -rf /opt/hermes/tmp/hermes-HERMES-BUILD line in /opt/hermes/scripts/update_hermes.sh script to remove existing failed updates

--- dirstructure/opt/hermes/scripts/update_hermes.sh
+++ dirstructure/opt/hermes/scripts/update_hermes.sh
@@ -1,3 +1,5 @@
+/bin/rm -rf /opt/hermes/tmp/hermes-HERMES-BUILD
+
 /bin/mkdir /opt/hermes/tmp/hermes-HERMES-BUILD
 
 /bin/tar -xvzf /opt/hermes/tmp/hermes-HERMES-BUILD.tar.gz -C /opt/hermes/tmp/hermes-HERMES-BUILD
