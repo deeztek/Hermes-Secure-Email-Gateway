03578eb05c8b36a7ebf8708dfdb68753863828cd
deeztek <dino.edwards@mydirectmail.net>
2023-10-15 09:01:52 -0400

    done

--- dirstructure/opt/hermes/scripts/nextcloud_create_email_profile.sh
+++ dirstructure/opt/hermes/scripts/nextcloud_create_email_profile.sh
@@ -1,1 +1,1 @@
-docker exec -u www-data nextcloud /var/www/html/occ mail:account:create THE-USERNAME "FULL-NAME" THE-EMAIL mail.hosting.deeztek.com 993 ssl postmaster@domain.tld ***REDACTED-CRED*** mail.hosting.deeztek.com 587 tls postmaster@domain.tld ***REDACTED-CRED*** password
+docker exec -u www-data nextcloud /var/www/html/occ mail:account:create THE-USERNAME "FULL-NAME" THE-EMAIL THE-IMAP-SERVER 993 ssl THE-IMAP-USERNAME THE-IMAP-PASSWORD THE-SMTP-SERVER 587 tls THE-SMTP-USERNAME THE-SMTP-PASSWORD password
