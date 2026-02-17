8b571b9a3b9a15e6a1d4bb60e51d57153e4e44b2
deeztek <dino.edwards@mydirectmail.net>
2026-02-17 05:40:12 -0500

    Fix generate_dkim.sh script template
    - Changed opendkim-genkey path from /usr/bin to /usr/sbin
    - Added proper #!/bin/bash shebang
    - Fixed incorrect use of -s flag twice; now uses -D for output directory
    - Added mv commands to rename files to expected naming convention
      (selector_domain.dkim.private and selector_domain.dkim.txt)

--- config/hermes/opt/hermes/scripts/generate_dkim.sh
+++ config/hermes/opt/hermes/scripts/generate_dkim.sh
@@ -1,1 +1,4 @@
-/usr/bin/opendkim-genkey -b THE-KEY -s THE-SELECTOR -d THE-DOMAIN -s /opt/hermes/dkim/keys/THE-SELECTOR_THE-DOMAIN.dkim
+#!/bin/bash
+/usr/sbin/opendkim-genkey -b THE-KEY -s THE-SELECTOR -d THE-DOMAIN -D /opt/hermes/dkim/keys/
+mv /opt/hermes/dkim/keys/THE-SELECTOR.private /opt/hermes/dkim/keys/THE-SELECTOR_THE-DOMAIN.dkim.private
+mv /opt/hermes/dkim/keys/THE-SELECTOR.txt /opt/hermes/dkim/keys/THE-SELECTOR_THE-DOMAIN.dkim.txt
